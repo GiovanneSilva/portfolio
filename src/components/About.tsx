de1ea784971f8a23b72164977128d5e9a18657d9
Giovanne Silva Braz Barbosa <vani.zito@gmail.com>
2025-01-05 03:32:20 -0300

    Photo correction

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,9 +7,9 @@ const About = () => {
 
   const ProfilePicture = () => (
     <div className="w-32 h-32 rounded-full overflow-hidden mx-auto mb-6">
-      <img src="src\components\images\me.jpg" alt="Profile" className="w-full h-full object-cover" />
+      <img src="/me.jpg" alt="Profile" className="w-full h-full object-cover" />
     </div>
-  );
+  );  
 
   const career = [
     {
@@ -37,6 +37,7 @@ const About = () => {
   return (
     <section id="about" className="py-20 bg-white dark:bg-gray-900">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        
         {/* Foto de Perfil */}
         <ProfilePicture />
 
